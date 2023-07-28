ef82b98b61b028697138ecb2257679b253050f1b
Lexxvasquez <122750837+Lexxvasquez@users.noreply.github.com>
2023-07-28 17:10:11 -0400

    finished index.js

--- seeds/index.js
+++ seeds/index.js
@@ -3,3 +3,20 @@ const seedPosts = require('./postseeds');
 const seedComments = require('./commentseeds');
 
 const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+  await seedUsers();
+  console.log('\n----- USERS SEEDED -----\n');
+
+  await seedPosts();
+  console.log('\n----- POSTS SEEDED -----\n');
+
+  await seedComments();
+  console.log('\n----- COMMENTS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+seedAll();
