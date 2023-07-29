9d3b8c6d6e6ce94f2e50e9067ac04f064ea87cf4
Lexxvasquez <122750837+Lexxvasquez@users.noreply.github.com>
2023-07-29 11:11:24 -0400

    connected server, added new files

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "secret_key",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("./public"));
+
+app.use(require('./controllers'));
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
